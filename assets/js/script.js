bd89196f6b901320d704138b0d03ba8d9053a9d6
Gordon <gordonjackman@gmail.com>
2023-11-28 16:44:18 -0500

    need new buttons to do anything.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
-var button = document.getElementById('button')
+var searchBtn = document.getElementById('button')
 var userInput = document.getElementById('form1');
-
+var table = document.getElementById('table');
+var existingSaved = JSON.parse(localStorage.getItem('saved'))??[];
 
 // function search() {
 //     var apiKey = "c994a9739d6f85220d0662ea56c8f64b";
@@ -14,25 +15,50 @@ var userInput = document.getElementById('form1');
 
 // }
 
-button.addEventListener('click', function() {
-    const fetchpromise = fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + userInput.value + "&units=imperial&appid=c994a9739d6f85220d0662ea56c8f64b");
-    fetchpromise.then((response) => response.json());
-    fetchpromise.then((response) => {
-        console.log(response);
+searchBtn.addEventListener('click', function() {
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + userInput.value + "&units=imperial&exclude=hourly,minutely&appid=c994a9739d6f85220d0662ea56c8f64b").then
+    (response => response.json()).then(data => {
+        console.log(data);
+        showData(data);
     })
-    fetchpromise.then(data => {
-        for (let i = 0; i < 5; i++) {
-           document.getElementById('day' + (i+1)).innerHTML = data.list[i].dt;
-           document.getElementById('image' + (i+1)).src ="https://openweathermap.org/img/wn/" +data.list[i].weather.icon+ ".png"
-           document.getElementById('temp' +(i+1)).innerHTML = data.list[i].main.temp;
-           document.getElementById('humid' +(i+1)).innerHTML = data.list[i].main.humidity;
-           document.getElementById('wind' +(i+1)).innerHTML = data.list[i].wind.speed
-            
-        }
-    })
+})    
 
-    console.log(userInput.value);
-    localStorage.setItem('city', userInput.value)
-})
-    
-    
+function showData(data) {
+    for (let i = 0; i < 6; i++) {
+               document.getElementById('day' + (i+1)).innerHTML = data.list[i].dt_txt;
+               document.getElementById('image' + (i+1)).src = ("https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png")
+               document.getElementById('temp' +(i+1)).innerHTML = "Temp:  " + data.list[i].main.temp + ' F';
+               document.getElementById('humid' +(i+1)).innerHTML = "Humidity:  " + data.list[i].main.humidity + '%';
+               document.getElementById('wind' +(i+1)).innerHTML = "Wind Speed:  " + data.list[i].wind.speed + "MPH";
+                
+            }
+        document.getElementById('city').innerHTML = userInput.value;
+        savesearch();
+        }
+
+function savesearch() {
+    var savedCity = userInput.value;
+    var existingSaved = JSON.parse(localStorage.getItem('saved'))??[];
+    var newSaved = existingSaved.concat(savedCity)
+    localStorage.setItem('saved', JSON.stringify(newSaved));
+    for (var i = 0; i < newSaved.length; i++) {
+        var li = document.createElement('li');
+        var newButton = document.createElement("button");
+        newButton.setAttribute('class', 'btn')
+        newButton.setAttribute('type', 'button')
+        newButton.innerHTML = newSaved[i];
+        li.appendChild(newButton);
+        table.appendChild(li);
+
+}}
+
+
+// button.addEventListener('click', function() {
+//     fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + newButton.value + "&units=imperial&exclude=hourly,minutely&appid=c994a9739d6f85220d0662ea56c8f64b").then
+//     (response => response.json()).then(data => {
+//         console.log(data);
+//         showData(data);
+//     })
+// })    
+
+
